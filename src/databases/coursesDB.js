f01d333192e82aab5188e694f019769394700b37
whiskacz <pawelblasiak1985@gmail.com>
2023-08-03 23:14:26 +0200

    React Practice r27

---
+++ src/databases/coursesDB.js
@@ -0,0 +1,21 @@
+import { githubImg } from 'https://images.freeimages.com/fic/images/icons/2779/simple_icons/4096/github_4096_black.png';
+import { pythonImg } from "https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png"
+import { javascriptImg } from "https://w7.pngwing.com/pngs/793/545/png-transparent-javascript-programmer-node-js-web-application-markup-language-thumbnail.png"
+
+export const coursesDB = [
+    {
+        title: "github",
+        price: 15,
+        img: githubImg
+    },
+    {
+        title: "javascript",
+        price: 20,
+        img: javascriptImg
+    },
+    {
+        title: "python",
+        price: 25,
+        img: pythonImg
+    }
+]
---
+++ src/databases/currencyDB.js
@@ -0,0 +1,17 @@
+export const currencyDB = [
+    {
+        title: "Euro",
+        code: "€",
+        rate: 1.5
+    },
+    {
+        title: "Dolar",
+        code: "$",
+        rate: 1.4
+    },
+    {
+        title: "Zloty",
+        code: "PLN",
+        rate: 5.5
+    }
+]
